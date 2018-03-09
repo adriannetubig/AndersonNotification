481d9af49c5504b40e06039164e65f1432a17df4
Adrianne Claude Tubig - MSI <adriannetubig@gmail.com>
2018-03-09 13:33:58 +0800

    Fixed unity config Fixed api authentication Moved css code from cshtml to site.css Removed client side package from nuget packages (already in bower) https://trello.com/c/QrhEIA4m/208-move-code-on-default-details-from-model-to-function   Moved code that sets the default values from model to function. Created code to only return the values inputed by user/consumer https://trello.com/c/qXS8Aj0X/207-2018-03-09-create-postman-for-andersonnotification   Created postman for logging in and sending email

--- AndersonNotification/AndersonNotificationWeb/Startup.cs
+++ AndersonNotification/AndersonNotificationWeb/Startup.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
 using System.Web.Mvc;
-using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
+using ExternalAccountWebAuthentication.Authentication;
 
 namespace AndersonNotificationWeb
 {
@@ -13,17 +12,25 @@ namespace AndersonNotificationWeb
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
-            //ConfigureCookie(app);   
-        }   
+            ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            AreaRegistration.RegisterAllAreas();
+            app.UseWebApi(config);
+        }
 
-        //public void ConfigureCookie(IAppBuilder app)
-        //{
-        //    app.UseCookieAuthentication(new CookieAuthenticationOptions
-        //    {
-        //        AuthenticationType = "ApplicationCookie",
-        //        LoginPath = new PathString("/Credential/Login")
-        //    });
-        //}
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(24),
+                Provider = new AuthorizationProvider()
+            };
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
     }
 }
